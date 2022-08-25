40602294de0d124828eec6ff1f0e247c43066b78
Hawken <HawkenSly@gmail.com>
2022-08-25 17:48:06 -0400

    Create argv.js

---
+++ argv.js
@@ -0,0 +1,14 @@
+const message1 = process.argv[0];
+const message2 = process.argv[1];
+const message3 = process.argv[2];
+
+//node .\argv.js testing!
+
+//console.log(`${message1}`);
+//C:\Program Files\nodejs\node.exe
+//console.log(`${message2}`);
+//D:\coding\Projects\Javascript\Node\testing\node-playground\argv.js
+//console.log(`${message3}`);
+//testing!
+
+
